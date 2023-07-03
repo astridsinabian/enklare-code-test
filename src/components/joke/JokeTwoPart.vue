c814b16183b1948a546ffea1120c9ca964c5cb27
Astrid Sinabian <astridsinabian@Arins-MacBook-Pro-Retina.local>
2023-07-03 18:44:43 +0200

    Clean up and created some base components

---
+++ src/components/joke/JokeTwoPart.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<template>
+  <div>Joke Two Part</div>
+</template>
+
+<style scoped></style>
